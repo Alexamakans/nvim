58653e96dbb4b5718312e379f5a855ee5fef149c
Alexamakans <fougner.alexander@gmail.com>
2025-10-21 21:36:37 +0200

    feat(nvim-python): add treesitter parsers, markdownlint and prettierd

--- nvim-python/init.lua
+++ nvim-python/init.lua
@@ -2,22 +2,34 @@ local base_dir = vim.fs.normalize(vim.fs.joinpath(vim.fn.stdpath("config"), ".."
 vim.opt.runtimepath:prepend(base_dir)
 
 local lsps = require("shared.lsps")
+local treesitters = require("shared.treesitters")
+
+vim.list_extend(treesitters, { "python", "toml", "yaml", "json", "markdown" })
 
 vim.list_extend(lsps, {
   {
     name = "basedpyright",
     config = {},
+  },
+  {
+    name = "tombi", -- toml lsp/linter/formatter
+    config = {},
+  },
+  {
     tools = {
-      -- configure ruff with .ruff.toml or pyproject.toml file in project repo
-      mason = { "ruff" },
+      mason = { "ruff", "markdownlint", "prettierd" },
       formatters = {
         by_ft = {
           python = { "ruff_fix", "ruff_format" },
+          markdown = { "markdownlint" },
+          yaml = { "prettierd" },
+          json = { "prettierd" },
         },
       },
       linters = {
         by_ft = {
           python = { "ruff" },
+          markdown = { "markdownlint" },
         },
       },
     },
